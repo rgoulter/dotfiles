22ab1ad2c3add9c4cfefcde920045ed5faf652d3
Richard Goulter <richard.goulter@gmail.com>
2018-02-23 15:10:43 +0800

    update emacs.el with recommendations from a blogpost

--- emacs.el
+++ emacs.el
@@ -14,10 +14,30 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; Use ibuffer mode, which manages buffers like Dired manages directories.
-;; From http://tuhdo.github.io/emacs-tutor.html
+;; ibuffer mode
+;;  this manages buffers like Dired manages directories.
+;;
+;; from: http://tuhdo.github.io/emacs-tutor.html
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
+;; TODO: Consider the merits of each of these. (vs using Custom to do it).
+;; from: https://sam217pa.github.io/2016/09/02/how-to-build-your-own-spacemacs/
+; (setq delete-old-versions -1 )		; delete excess backup versions silently
+; (setq version-control t )		; use version control
+; (setq vc-make-backup-files t )		; make backups file even when in version controlled dir
+; (setq backup-directory-alist `(("." . "~/.emacs.d/backups")) ) ; which directory to put backups file
+; (setq vc-follow-symlinks t )				       ; don't ask for confirmation when opening symlinked file
+; (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)) ) ;transform backups file name
+; (setq inhibit-startup-screen t )	; inhibit useless and old-school startup screen
+; (setq ring-bell-function 'ignore )	; silent bell when you make a mistake
+; (setq coding-system-for-read 'utf-8 )	; use utf-8 by default
+; (setq coding-system-for-write 'utf-8 )
+; (setq sentence-end-double-space nil)	; sentence SHOULD end with only a point.
+; (setq default-fill-column 80)		; toggle wrapping text at the 80th character
+; (setq initial-scratch-message "Welcome in Emacs") ; print a default message in the empty scratch buffer opened at startup
+
+;; Reference: https://stackoverflow.com/questions/14302171/ctrlu-in-emacs-when-using-evil-key-bindings
+(setq evil-want-C-u-scroll t)
 
 (require 'package)
   (push '("marmalade" . "http://marmalade-repo.org/packages/")
@@ -32,25 +52,116 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
+;; Bootstrap `req-package'
+(unless (package-installed-p 'use-package) ; unless it is already installed
+  (package-refresh-contents) ; updage packages archive
+  (package-install 'req-package)) ; and install the most recent version of use-package
+
+;; `req-package` is different than `use-package`,
+;; in that `req-package` also lets this emacs config declare which packages
+;;  are dependencies of a required package.
 (require 'req-package)
+
+(req-package org)
 
 (req-package evil
   :config (evil-mode 1))
 
-;; https://github.com/sellout/emacs-color-theme-solarized
-; 930-stars on GitHub
+(req-package evil-leader)
+
+(req-package evil-org)
+
+;; Rainbow Delimiters
+;; Colours are pretty. :-)
+;; GitHub: https://github.com/istib/rainbow-blocks
+;;
+;; Usage-Notes:
+;; - use M-x rainbow-delimiters-mode to toggle.
+;; - EVIL: Can use as Ex command.
+(req-package rainbow-delimiters
+  ; Enable for most programming languages:
+  :config (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode))
+
+;; GitHub: https://github.com/sellout/emacs-color-theme-solarized
+;; 930-stars on GitHub
 (req-package color-theme-solarized
   :require color-theme
   :config (progn
             ; (set-frame-parameter nil 'background-mode 'dark) ; for GUI-only. I guess this breaks in term.
             (load-theme 'solarized t)))
 
-;; 'Borrowed' by searching GitHub for "use-package tuareg"
+;; Ivy/Counsel/Swiper work together.
+;; Recommended by: https://sam217pa.github.io/2016/09/02/how-to-build-your-own-spacemacs/
+;; GitHub: https://github.com/abo-abo/swiper
+
+;; Ivy is comparable to Helm.
+;; TODO: Look up (or try) Ivy vs Helm.
+(req-package ivy)
+
+(req-package counsel)
+
+(req-package swiper)
+
+(req-package avy)
+
+(req-package which-key)
+
+;; Taken from: https://sam217pa.github.io/2016/09/02/how-to-build-your-own-spacemacs/
+;; GitHub: https://github.com/noctuid/general.el
+(req-package general
+  :require avy
+  :config
+  (general-define-key "C-'" 'avy-goto-word-1)
+  (general-define-key
+   ;; replace default keybindings
+   "C-s" 'swiper             ; search for string in current buffer
+   "M-x" 'counsel-M-x        ; replace default M-x with ivy backend
+   )
+  (general-define-key
+   :prefix "C-c"
+   ;; bind to simple key press
+    "b"	'ivy-switch-buffer  ; change buffer, chose using ivy
+    "/"   'counsel-git-grep   ; find string in git project
+    ;; bind to double key press
+    "f"   '(:ignore t :which-key "files")
+    "ff"  'counsel-find-file  ; find file using ivy
+    "fr"	'counsel-recentf    ; find recently edited files
+    "p"   '(:ignore t :which-key "project")
+    "pf"  '(counsel-git :which-key "find file in git dir")        ; find file in git project
+    )
+;  (general-define-key
+;   :states '(normal visual insert emacs)
+;   :prefix "SPC"
+;   :non-normal-prefix "C-SPC"
+;
+;    ;; simple command
+;    "'"   '(iterm-focus :which-key "iterm")
+;    "?"   '(iterm-goto-filedir-or-home :which-key "iterm - goto dir")
+;    "/"   'counsel-ag
+;    "TAB" '(switch-to-other-buffer :which-key "prev buffer")
+;    "SPC" '(avy-goto-word-or-subword-1  :which-key "go to char")
+;
+;    ;; Applications
+;    "a" '(:ignore t :which-key "Applications")
+;    "ar" 'ranger
+;    "ad" 'dired)
+  )
+
+;; For Editing Language: Haskell
+(req-package haskell-mode
+  :init
+  (add-hook 'haskell-mode-hook 'turn-on-haskell-indent))
+
+;; Tuarag Mode
+;; For Editing Language: OCaml
 (req-package tuareg
   :mode (("\\.ml[ily]?$" . tuareg-mode)
          ("\\.topml$" . tuareg-mode)
          ("\\.topscript$" . tuareg-mode)))
 
+;; Merlin
+;; OCaml completion
+;; For Editing Language: OCaml
 (req-package merlin
   :init (setq ; merlin-use-auto-complete-mode t
               merlin-error-after-save nil)
@@ -64,21 +175,7 @@
   ;        ("C-c <down>" . merlin-type-enclosing-go-down))
   )
 
-(req-package haskell-mode
-  :init
-  (add-hook 'haskell-mode-hook 'turn-on-haskell-indent))
-
-; https://github.com/istib/rainbow-blocks looks maybe pretty, also?
-; use M-x rainbow-delimiters-mode to toggle. (EVIL: Can use as Ex command).
-(req-package rainbow-delimiters
-  ; Enable for most programming languages:
-  :config (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode))
-
-(req-package org)
-(req-package evil-leader)
-(req-package evil-org)
-
-; Qn: Why is it (add-hook ... #'whatever-mode) vs (add-hook ... 'whatever-mode) ???
+; QUESTION: Why is it (add-hook ... #'whatever-mode) vs (add-hook ... 'whatever-mode) ???
 
 (req-package-finish)
 
@@ -94,8 +191,8 @@
 
 
 ; Set to dark or light
-;? These won't work if use `M-x customize-variable frame-background-mode`.
-;? What's an idiomatic way of enabling something like this?
+; - QUESTION: These won't work if use `M-x customize-variable frame-background-mode`.
+; - QUESTION: What's an idiomatic way of enabling something like this?
 (defun solarized-light ()
   (interactive)
   (set-frame-parameter nil 'background-mode 'light)
@@ -110,4 +207,9 @@
 ; Disable the toolbar;
 ; can re-enable with `M-x tool-bar-mode`
 (tool-bar-mode -1)
+
+
+; Apparently needed for emacs-org-mode code to look pretty
+; https://orgmode.org/worg/org-contrib/babel/examples/fontify-src-code-blocks.html
+(setq org-src-fontify-natively t)
 
