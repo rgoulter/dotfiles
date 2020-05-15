66af23e3b146729fa31ec2891f46f737d3eb4494
Richard Goulter <richard.goulter@gmail.com>
2020-05-15 22:14:49 +0700

    emacs: files for chemacs to run other emacs profiles/distributions

---
+++ emacs-profiles.el
@@ -0,0 +1,5 @@
+(("default" . ((user-emacs-directory . "~/.emacs.d")))
+ ("spacemacs" . ((user-emacs-directory . "~/spacemacs")))
+ ("prelude" . ((user-emacs-directory . "~/prelude")))
+ ("doom" . ((user-emacs-directory . "~/doom-emacs")
+            (env . (("DOOMDIR" . "~/.doom.d"))))))
