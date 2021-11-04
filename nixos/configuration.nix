a96b0e5d67c623a06226dbf6f3065ef04023f1d2
Richard Goulter <richard.goulter@gmail.com>
2021-11-04 07:56:59 +0700

    nixos: update bluetooth audio config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -12,15 +12,23 @@
 
   # https://nixos.wiki/wiki/Bluetooth#Using_Bluetooth_headsets_with_PulseAudio
   # https://nixos.wiki/wiki/Bluetooth#Enabling_A2DP_Sink
-  hardware.bluetooth = {
-    enable = true;
-    settings = {
-      General = {
-        Enable = "Source,Sink,Media,Socket";
+  hardware = {
+    bluetooth = {
+      enable = true;
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+        };
       };
     };
-  };
-  hardware.pulseaudio.enable = true;
+    pulseaudio.enable = false;
+  }
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-extra
+    open-dyslexic
+  ];
 
   # https://github.com/NixOS/nix/issues/2208
   nix.extraOptions = "keep-outputs = true";
@@ -29,15 +37,59 @@
 
   programs.ssh.startAgent = true;
 
+  # https://nixos.wiki/wiki/PipeWire
+  # rtkit is optional but recommended
+  security.rtkit.enable = true;
+
   services = {
     # Enable the OpenSSH daemon.
     openssh.enable = true;
+
+    # https://nixos.wiki/wiki/PipeWire
+    # https://nixos.wiki/wiki/PipeWire#Bluetooth_Configuration
+    pipewire  = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      media-session.config.bluez-monitor.rules = [
+        {
+          # Matches all cards
+          matches = [ { "device.name" = "~bluez_card.*"; } ];
+          actions = {
+            "update-props" = {
+              "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
+              # mSBC: higher quality in calls.
+              # mSBC is not expected to work on all headset + adapter combinations.
+              "bluez5.msbc-support" = true;
+              # SBC-XQ: higher audio
+              # SBC-XQ is not expected to work on all headset + adapter combinations.
+              "bluez5.sbc-xq-support" = true;
+            };
+          };
+        }
+        {
+          matches = [
+            # Matches all sources
+            { "node.name" = "~bluez_input.*"; }
+            # Matches all outputs
+            { "node.name" = "~bluez_output.*"; }
+          ];
+          actions = {
+            "node.pause-on-idle" = false;
+          };
+        }
+      ];
+    };
 
     udev = {
       # https://docs.qmk.fm/#/faq_build?id=linux-udev-rules
       extraRules = ''
 # Atmel DFU
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", TAG+="uaccess"
+
+# STM32 DFU
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", TAG+="uaccess"
       '';
       packages = [ pkgs.yubikey-personalization ];
     };
