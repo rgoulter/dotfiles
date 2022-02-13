8d132d8549b393c173932e7a5184fc50b988e08c
Richard Goulter <richard.goulter@gmail.com>
2022-02-13 11:09:25 +0700

    nixos: add STM32duino bootloader to udev rules

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -92,6 +92,9 @@ SUBSYSTEMS=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="2ff4", TAG+="uacc
 
 # STM32 DFU
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", TAG+="uaccess"
+
+# STM32duino 1eaf:0003
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="1eaf", ATTRS{idProduct}=="0003", TAG+="uaccess"
       '';
       packages = [ pkgs.yubikey-personalization ];
     };
