350aefb27c3fadc5c7f189d38e59abc50d1f6f46
Richard Goulter <richard.goulter@gmail.com>
2022-04-09 21:41:03 +0700

    doom emacs: try this

--- doom/packages.el
+++ doom/packages.el
@@ -8,12 +8,14 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
+(package! plantuml-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
+
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
