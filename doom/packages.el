c2083aa5107cda749c4db64cac0f3c8872cb8163
Richard Goulter <richard.goulter@gmail.com>
2022-05-12 00:52:24 +0700

    doom emacs: install some additional packages

--- doom/packages.el
+++ doom/packages.el
@@ -10,12 +10,19 @@
 ;(package! some-package)
 (package! plantuml-mode)
 
+;; zettel-deft
+(package! ranger)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
+(package! discover-my-major
+ :recipe (:host github :repo "jguenther/discover-my-major"))
 
+(package! zetteldeft
+ :recipe (:host github :repo "EFLS/zetteldeft"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
