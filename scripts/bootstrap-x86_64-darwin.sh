0e75c48eb1812f91003f3988eb449ab04dec3445
Richard Goulter <richard.goulter@gmail.com>
2022-10-05 00:14:11 +0700

    scripts: refactor bootstrap HM scripts

--- scripts/bootstrap-x86_64-darwin.sh
+++ scripts/bootstrap-x86_64-darwin.sh
@@ -2,11 +2,15 @@
 
 set -ex
 
+FLAKE_URI="."
 SYSTEM="x86_64-darwin"
-FLAKE_OUTPUT=".#homeConfigurations.richardgoulter-${SYSTEM}.activationPackage"
+HM_USERNAME="richardgoulter"
+HM_CONFIG="${HM_USERNAME}-${SYSTEM}"
+FLAKE_OUTPUT="${FLAKE_URI}#homeConfigurations.${HM_CONFIG}.activationPackage"
+
 nix build --no-link "${FLAKE_OUTPUT}"
 
 "$(nix path-info ${FLAKE_OUTPUT})"/activate
 
 # and switch with:
-# home-manager switch --flake '.#rgoulter-x86_64-darwin'
+# home-manager switch --flake '.#richardgoulter-x86_64-darwin'
--- scripts/bootstrap-x86_64-linux.sh
+++ scripts/bootstrap-x86_64-linux.sh
@@ -2,11 +2,15 @@
 
 set -ex
 
+FLAKE_URI="."
 SYSTEM="x86_64-linux"
-FLAKE_OUTPUT=".#homeConfigurations.rgoulter-${SYSTEM}.activationPackage"
+HM_USERNAME="rgoulter"
+HM_CONFIG="${HM_USERNAME}-${SYSTEM}"
+FLAKE_OUTPUT="${FLAKE_URI}#homeConfigurations.${HM_CONFIG}.activationPackage"
+
 nix build --no-link "${FLAKE_OUTPUT}"
 
 "$(nix path-info ${FLAKE_OUTPUT})"/activate
 
 # and switch with:
-# home-manager switch --flake '.#rgoulter-x86_64-darwin'
+# home-manager switch --flake '.#rgoulter-x86_64-linux'
