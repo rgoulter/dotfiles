b1b48badd40dcf23f36669d813c70c88763a7571
Richard Goulter <richard.goulter@gmail.com>
2025-04-24 11:39:28 +0700

    doom: packages: add gptel

--- doom/packages.el
+++ doom/packages.el
@@ -35,6 +35,8 @@
 
 (package! writeroom-mode)
 
+(package! gptel :recipe (:nonrecursive t))
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
