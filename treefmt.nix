a1821fe78614cdc8f9dd2c548cafbd6e5ae711dd
Richard Goulter <richard.goulter@gmail.com>
2026-06-21 20:30:16 +0700

    Expand treefmt formatters and drop legacy treefmt.toml
    Enable fish_indent, kdlfmt, taplo, and yamlfmt in treefmt.nix with
    2-space kdlfmt settings. Remove the old treefmt.toml now that flake
    treefmt-nix owns formatter configuration.

--- treefmt.nix
+++ treefmt.nix
@@ -1,6 +1,20 @@
 {pkgs, ...}: {
   projectRootFile = "flake.nix";
   programs.alejandra.enable = true;
+  programs.fish_indent.enable = true;
+  programs.kdlfmt.enable = true;
   programs.shellcheck.enable = true;
   programs.shfmt.enable = true;
+  programs.taplo.enable = true;
+  programs.yamlfmt.enable = true;
+
+  settings.formatter.kdlfmt.options = let
+    kdlfmtConfig = pkgs.writeText "kdlfmt.kdl" ''
+      indent_size 2
+      use_tabs false
+    '';
+  in [
+    "--config"
+    "${kdlfmtConfig}"
+  ];
 }
