641fedac3ebc3d8f6ddf13731e8260d3d908e016
Richard Goulter <richard.goulter@gmail.com>
2026-07-12 15:23:47 +0700

    doom: remove copilot

--- doom/config.el
+++ doom/config.el
@@ -142,15 +142,6 @@
                     :height 140
                     :italic t))))
 
-;; accept completion from copilot and fallback to company
-(use-package! copilot
-  :hook (prog-mode . copilot-mode)
-  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
-         ("C-<tab>" . 'copilot-accept-completion-by-word)
-         :map copilot-completion-map
-         ("<tab>" . 'copilot-accept-completion)
-         ("TAB" . 'copilot-accept-completion)))
-
 (use-package! discover-my-major
  :config
  (map! :leader :n "h C-m" 'discover-my-major))
